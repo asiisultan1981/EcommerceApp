367b62fa30fa89ed2cf5e0516632303cff6c423d
Asif Sultan <asii03455900863@gmail.com>
2020-08-28 17:41:47 +0500

    second commit

--- app/src/main/java/com/example/a8mm/Retrofit/ApiClient.java
+++ app/src/main/java/com/example/a8mm/Retrofit/ApiClient.java
@@ -6,7 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;  fffff
 
 public class ApiClient {
      public static final String BASE_URL = "http://192.168.100.242:80/80/";
